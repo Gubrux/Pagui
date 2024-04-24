a3c920e2dcfcfdac90348c8bc1c507ec3dd0a7b4
Gubrux <gabrielcentur@gmail.com>
2024-04-23 21:14:02 -0400

    wrapped the pageRouter

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import PageRouter from "./PageRouter";
 
-import PageRouter from './PageRouter';
+import { EventProvider } from "./context/EventContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <PageRouter />
-  </React.StrictMode>
+    <React.StrictMode>
+        <EventProvider>
+            <PageRouter />
+        </EventProvider>
+    </React.StrictMode>
 );
-
